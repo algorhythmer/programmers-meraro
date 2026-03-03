a338afb110f9a94ab08acf9c7123cf621e44e056
Meraro <118741054+Meraro@users.noreply.github.com>
2026-03-03 21:39:09 +0900

    [level 2] Title: 피로도, Time: 0.65 ms, Memory: 9.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,15 +1,21 @@
 from itertools import *
 
 def solution(k, dungeons):
-    
+    l = len(dungeons)
+    dp = [-1] * (1 << l)
+    dp[0] = k
     answer = 0
-    for order in permutations(dungeons, len(dungeons)):
-        n = 0
-        m = k
-        for x, y in order:
-            if m >= x:
-                m -= y
-                n += 1
-        answer = max(answer, n)
+    
+    for visited_bits in range(1<<l):
+        if dp[visited_bits] == -1 : continue
+        
+        curr = bin(visited_bits).count('1')
+        answer = max(answer, curr)        
+        
+        for i in range(l):
+            if visited_bits & (1<<i): continue
+            if dp[visited_bits]  < dungeons[i][0]: continue
+            n = visited_bits | (1 << i) 
+            dp[n] = max(dp[n], dp[visited_bits] - dungeons[i][1])
     
     return answer
